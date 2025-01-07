3ecc853908b662aa0100d0b74f5796222a69de83
Dastan Ibraimov <ibraimovdastan@gmail.com>
2025-01-07 21:57:38 +0600

    Создал модель артикл зарегистрировал в адмиинке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author', 'published_at']
+    list_filter = ['published_at', 'author']
+    date_hierarchy = 'published_at'
+    search_fields = ['title', 'content']
+    readonly_fields = ['published_at', 'updated_at']
+
+
+admin.site.register(Article, ArticleAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    author = models.CharField(max_length=50, verbose_name='Author name', default='Anonymous')
+    title = models.CharField(max_length=100, verbose_name='Article title')
+    content = models.TextField()
+    published_at = models.DateTimeField(auto_now_add=True, verbose_name='Publication date')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated date')
+
+    def __str__(self):
+        return f'{self.pk}. {self.title}: {self.author}'
+
+    class Meta:
+        db_table = 'articles'
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
